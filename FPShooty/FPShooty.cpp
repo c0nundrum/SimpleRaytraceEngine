6b7a48a654b34ff7a38d36ab92054bcc79fc6fe7
c0nundrum <edge.of.vfx@gmail.com>
2019-07-22 00:33:08 -0300

    Working textured walls and shooting!

--- FPShooty/FPShooty.cpp
+++ FPShooty/FPShooty.cpp
@@ -12,77 +12,80 @@
 //Based On that awesome tutorial, with a few hacks
 
 #include "pch.h"
-#include <iostream>
-#include <chrono>
-#include <vector>
 #include <utility>
 #include <algorithm>
 using namespace std;
 
-#include <Windows.h>
+#include "olcConsoleGameEngine.h"
 
-int nScreenWidth = 120;
-int nScreenHeight = 40;
+class OneLoneCoder_UltimateFPS : public olcConsoleGameEngine {
+public:
+	OneLoneCoder_UltimateFPS() {
+		m_sAppName = L"Ultimate First Person Shooter";
+	}
 
-float fPlayerX = 8.0f;
-float fPlayerY = 8.0f;
-float fPlayerA = 0.0f;
+	virtual bool OnUserCreate() {
 
-int nMapHeight = 16;
-int nMapWidth = 16;
+		map += L"#########.......#########.......";
+		map += L"#...............#...............";
+		map += L"#.......#########.......########";
+		map += L"#..............##..............#";
+		map += L"#......##......##......##......#";
+		map += L"#......##..............##......#";
+		map += L"#..............##..............#";
+		map += L"###............####............#";
+		map += L"##.............###.............#";
+		map += L"#............####............###";
+		map += L"#..............................#";
+		map += L"#..............##..............#";
+		map += L"#..............##..............#";
+		map += L"#...........#####...........####";
+		map += L"#..............................#";
+		map += L"###..####....########....#######";
+		map += L"####.####.......######..........";
+		map += L"#...............#...............";
+		map += L"#.......#########.......##..####";
+		map += L"#..............##..............#";
+		map += L"#......##......##.......#......#";
+		map += L"#......##......##......##......#";
+		map += L"#..............##..............#";
+		map += L"###............####............#";
+		map += L"##.............###.............#";
+		map += L"#............####............###";
+		map += L"#..............................#";
+		map += L"#..............................#";
+		map += L"#..............##..............#";
+		map += L"#...........##..............####";
+		map += L"#..............##..............#";
+		map += L"################################";
 
-float fFOV = 3.14159 / 4.0;
-float fDepth = 16.0f;
+		spriteWall = new olcSprite(L"fps_wall1.spr");
+		spriteLamp = new olcSprite(L"fps_lamp1.spr");
+		spriteFireBall = new olcSprite(L"fps_fireball1.spr");
 
-int main()
-{
-	// Create Screen Buffer
-	wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
-	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	SetConsoleActiveScreenBuffer(hConsole);
-	DWORD dwBytesWritten = 0;
 
-	wstring map;
-	map += L"################";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..........#...#";
-	map += L"#..........#...#";
-	map += L"#..........#...#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#........#######";
-	map += L"#..............#";
-	map += L"################";
+		fDepthBuffer = new float[ScreenWidth()];
 
-	auto tp1 = chrono::system_clock::now();
-	auto tp2 = chrono::system_clock::now();
+		listObjects = {
+			{8.5f, 8.5f, 0.0f, 0.0f, false, spriteLamp},
+			{7.5f, 7.5f, 0.0f, 0.0f, false, spriteLamp},
+			{10.5f, 3.5f, 0.0f, 0.0f, false, spriteLamp},
+		};
 
-	while (1) {
+		return true;
+	}
 
-		tp2 = chrono::system_clock::now();
-		chrono::duration<float> elapsedTime = tp2 - tp1;
-		tp1 = tp2;
-
-		float fElapsedTime = elapsedTime.count();
+	virtual bool OnUserUpdate(float fElapsedTime) {
 
 		//Controls
-		//Handle CCw Rotation
-		//if (GetAsyncKeyState((unsigned short)'A' & 0x8000))
-		if (GetAsyncKeyState(0x41))//A key
+		if (m_keys[L'A'].bHeld)//A key
 			fPlayerA -= (0.8f) * fElapsedTime;
 
-		//if (GetAsyncKeyState((unsigned short)'D' & 0x8000))
-		if (GetAsyncKeyState(0x44))//D key
+		if (m_keys[L'D'].bHeld)//D key
 			fPlayerA += (0.8f) * fElapsedTime;
 
 		//W Key
-		if (GetAsyncKeyState(0x57)) {
+		if (m_keys[L'W'].bHeld) {
 			fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
 			fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
 
@@ -95,7 +98,7 @@ int main()
 		}
 
 		//S Key
-		if (GetAsyncKeyState(0x53)) {
+		if (m_keys[L'S'].bHeld) {
 			fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
 			fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
 
@@ -108,9 +111,50 @@ int main()
 
 		}
 
-		for (int x = 0; x < nScreenWidth; x++) {
+		//Strafe Right Key
+		if (m_keys[L'E'].bHeld) {
+			fPlayerX += cosf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerY -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+
+			if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#') {
+
+				fPlayerX -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+				fPlayerY += sinf(fPlayerA) * 5.0f * fElapsedTime;
+
+			}
+
+		}
+
+		//Strafe Left Key
+		if (m_keys[L'Q'].bHeld) {
+			fPlayerX -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerY += sinf(fPlayerA) * 5.0f * fElapsedTime;
+
+			if (map[(int)fPlayerY * nMapWidth + (int)fPlayerX] == '#') {
+
+				fPlayerX += cosf(fPlayerA) * 5.0f * fElapsedTime;
+				fPlayerY -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+
+			}
+
+		}
+
+		//Fire Bullets
+		if (m_keys[VK_SPACE].bReleased) {
+			sObject o;
+			o.x = fPlayerX;
+			o.y = fPlayerY;
+			float fNoise = (((float)rand() / (float)RAND_MAX) - 0.5f) * 0.1f;
+			o.vx = sinf(fPlayerA + fNoise) * 8.0f;
+			o.vy = cosf(fPlayerA + fNoise) * 8.0f;
+			o.sprite = spriteFireBall;
+			o.bRemove = false;
+			listObjects.push_back(o);
+		}
+
+		for (int x = 0; x < ScreenWidth(); x++) {
 			// For each column, calculate the profected ray angle into world space
-			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)ScreenWidth()) * fFOV;
 
 			float fDistanceToWall = 0;
 			bool bHitWall = false;
@@ -119,9 +163,11 @@ int main()
 			float fEyeX = sinf(fRayAngle);
 			float fEyeY = cosf(fRayAngle);
 
+			float fSampleX = 0.0f;
+
 			while (!bHitWall && fDistanceToWall < fDepth) {
 
-				fDistanceToWall += 0.1f;
+				fDistanceToWall += 0.01f;
 
 				int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
 				int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
@@ -132,30 +178,29 @@ int main()
 					bHitWall = true; // Just set the distance to maximum depth
 					fDistanceToWall = fDepth;
 
-				} else {
+				}
+				else {
 					//Ray is inbounds so test to see if the ray cell is a wall block
 					if (map[nTestY * nMapWidth + nTestX] == '#') {
 
 						bHitWall = true;
 
-						vector<pair<float, float>> p; //distance, dot
+						float fBlockMidX = (float)nTestX + 0.5f;
+						float fBlockMidY = (float)nTestY + 0.5f;
 
-						for (int tx = 0; tx < 2; tx++)
-							for (int ty = 0; ty < 2; ty++) {
-								float vy = (float)nTestY + ty - fPlayerY;
-								float vx = (float)nTestX + tx - fPlayerX;
-								float d = sqrt(vx * vx + vy * vy);
-								float dot = (fEyeX * vx / d) + (fEyeY * vy / d);
-								p.push_back(make_pair(d, dot));
-							}
+						float fTestPointX = fPlayerX + fEyeX * fDistanceToWall;
+						float fTestPointY = fPlayerY + fEyeY * fDistanceToWall;
 
-						// Sort Pairs from closest to farthest
-						sort(p.begin(), p.end(), [](const pair<float, float> &left, const pair<float, float> &right) {return left.first < right.first; });
+						float fTestAngle = atan2f((fTestPointY - fBlockMidY), (fTestPointX - fBlockMidX));
 
-						float fBound = 0.01f;
-						if (acos(p.at(0).second) < fBound) bBoundary = true;
-						if (acos(p.at(1).second) < fBound) bBoundary = true;
-						//if (acos(p.at(2).second) < fBound) bBoundary = true;
+						if (fTestAngle >= -3.14159f * 0.25f && fTestAngle < 3.14159f * 0.25f)
+							fSampleX = fTestPointY - (float)nTestY;
+						if (fTestAngle >= 3.14159f * 0.25f && fTestAngle < 3.14159f * 0.75f)
+							fSampleX = fTestPointX - (float)nTestX;
+						if (fTestAngle < -3.14159f * 0.25f && fTestAngle >= -3.14159f * 0.75f)
+							fSampleX = fTestPointX - (float)nTestX;
+						if (fTestAngle >= 3.14159f * 0.75f || fTestAngle < -3.14159f * 0.75f)
+							fSampleX = fTestPointY - (float)nTestY;
 
 					}
 				}
@@ -163,58 +208,149 @@ int main()
 			}
 
 			//Calculate distance to ceiling and floor
-			int nCeiling = (float)(nScreenHeight / 2.0f) - nScreenHeight / ((float)fDistanceToWall);
-			int nFloor = nScreenHeight - nCeiling;
+			int nCeiling = (float)(ScreenHeight() / 2.0f) - ScreenHeight() / ((float)fDistanceToWall);
+			int nFloor = ScreenHeight() - nCeiling;
+
+			fDepthBuffer[x] = fDistanceToWall;
 
 			short nShade = ' ';
 			short nFloorShade = ' ';
 
-			if (fDistanceToWall <= fDepth / 4.0f)		nShade = 0x2588; //Very Close
-			else if (fDistanceToWall < fDepth / 3.0f)	nShade = 0x2593;
-			else if (fDistanceToWall < fDepth / 2.0f)	nShade = 0x2592;
-			else if (fDistanceToWall < fDepth)			nShade = 0x2591;
-			else                                        nShade = ' '; //Very far away
+			
 
-			if (bBoundary)		nShade = ' ';
+			for (int y = 0; y < ScreenHeight(); y++) {
+				if (y <= nCeiling)//Ceiling
+					Draw(x, y, L' ');
+				else if (y > nCeiling && y <= nFloor)//Wall
+				{
+					if (fDistanceToWall < fDepth) {
+						float fSampleY = ((float)y - (float)nCeiling) / ((float)nFloor - (float)nCeiling);
+						Draw(x, y, spriteWall->SampleGlyph(fSampleX, fSampleY), spriteWall->SampleColour(fSampleX, fSampleY));
+					}
+					else {
+						Draw(x, y, PIXEL_SOLID, 0);
+					}
 
-			for (int y = 0; y < nScreenHeight; y++) {
-				if (y < nCeiling)
-					screen[y*nScreenWidth + x] = ' ';
-				else if (y > nCeiling && y <= nFloor)
-					screen[y*nScreenWidth + x] = nShade;
+				}
 				else // Floor
 				{
-					// Shade floor based on distance
-					float b = 1.0f - (((float)y - nScreenHeight / 2.0f) / ((float)nScreenHeight / 2.0f));
-					if (b < 0.25)		nFloorShade = '#';
-					else if (b < 0.5)	nFloorShade = 'x';
-					else if (b < 0.75)	nFloorShade = '.';
-					else if (b < 0.9)	nFloorShade = '-';
-					else				nFloorShade = ' ';
-					screen[y*nScreenWidth + x] = nFloorShade;
+					Draw(x, y, PIXEL_SOLID, FG_DARK_GREEN);
 				}
-					
+
 			}
 
 		}
 
-		// Display Stats
-		swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f ", fPlayerX, fPlayerY, fPlayerA, 1.0f / fElapsedTime);
+		//Update and draw objects
+		for (auto &object : listObjects) {
+
+			// Update Object Physics
+			object.x += object.vx * fElapsedTime;
+			object.y += object.vy * fElapsedTime;
+
+			// Check if object is inside wall - set flag for removal
+			if (map[(int)object.x * nMapWidth + (int)object.y] == '#')
+				object.bRemove = true;
+
+			//Can Object be seen?
+			float fVecX = object.x - fPlayerX;
+			float fVecY = object.y - fPlayerY;
+			float fDistanceFromPlayer = sqrtf(fVecX*fVecX + fVecY * fVecY);
+
+			//Calculate the angle between lamp and player feet
+			float fEyeX = sinf(fPlayerA);
+			float fEyeY = cosf(fPlayerA);
+			float fObjectAngle = atan2f(fEyeY, fEyeX) - atan2f(fVecY, fVecX);
+			if (fObjectAngle < -3.14159f)
+				fObjectAngle += 2.0f * 3.13159f;
+			if (fObjectAngle > 3.14159f)
+				fObjectAngle -= 2.0f * 3.13159f;
+
+			bool bInPlayerFOV = fabs(fObjectAngle) < fFOV / 2.0f;
+
+			if (bInPlayerFOV && fDistanceFromPlayer >= 0.5f && fDistanceFromPlayer < fDepth) {
+
+				float fObjectCeiling = (float)(ScreenHeight() / 2.0) - ScreenHeight() / ((float)fDistanceFromPlayer);
+				float fObjectFloor = ScreenHeight() - fObjectCeiling;
+				float fObjectHeight = fObjectFloor - fObjectCeiling;
+
+				float fObjectAspectRatio = (float)object.sprite->nHeight / (float)object.sprite->nWidth;
+				float fObjectWidth = fObjectHeight / fObjectAspectRatio;
+
+				float fMiddleOfObject = (0.5f * (fObjectAngle / (fFOV / 2.0f)) + 0.5f) * (float)ScreenWidth();
+
+				for (float lx = 0; lx < fObjectWidth; lx++) {
+					for (float ly = 0; ly < fObjectHeight; ly++) {
+						float fSampleX = lx / fObjectWidth;
+						float fSampleY = ly / fObjectHeight;
+						wchar_t c = object.sprite->SampleGlyph(fSampleX, fSampleY);
+						int nObjectColumn = (int)(fMiddleOfObject + lx - (fObjectWidth / 2.0f));
+						if (nObjectColumn >= 0 && nObjectColumn < ScreenWidth()) {
+							if (c != L' ' && fDepthBuffer[nObjectColumn] >= fDistanceFromPlayer) {
+								Draw(nObjectColumn, fObjectCeiling + ly, c, object.sprite->SampleColour(fSampleX, fSampleY));
+								fDepthBuffer[nObjectColumn] = fDistanceFromPlayer;
+							}
+
+						}
+					}
+				}
+
+			}
+
+		}
+
+		// Remove dead objects from object list
+		listObjects.remove_if([](sObject &o) {return o.bRemove; });
 
 		//Display Map
 		for (int nx = 0; nx < nMapWidth; nx++)
 			for (int ny = 0; ny < nMapHeight; ny++) {
-				screen[(ny + 1)*nScreenWidth + nx] = map[ny * nMapWidth + nx];
+				Draw(nx + 1, ny + 1, map[ny * nMapWidth + nx]);
 			}
+		Draw((int)fPlayerY + 1, (int)fPlayerX + 1, L'P');
 
-		screen[((int)fPlayerY + 1) * nScreenWidth + (int)fPlayerX] = 'P';
-
-		// Display Frame
-		screen[nScreenWidth * nScreenHeight - 1] = '\0';
-		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
-
+		return true;
 	}
 
+private:
+
+	float fPlayerX = 8.0f;
+	float fPlayerY = 8.0f;
+	float fPlayerA = 0.0f;
+	wstring map;
+
+	int nMapHeight = 32;
+	int nMapWidth = 32;
+
+	float fFOV = 3.14159 / 4.0;
+	float fDepth = 16.0f;
+
+	olcSprite *spriteWall;
+	olcSprite *spriteLamp;
+	olcSprite *spriteFireBall;
+
+	float *fDepthBuffer = nullptr;
+
+	struct sObject {
+		float x;
+		float y;
+		float vx;
+		float vy;
+		bool bRemove;
+		olcSprite *sprite;
+	};
+
+	list<sObject> listObjects;
+};
+
+
+
+int main()
+{
+
+	OneLoneCoder_UltimateFPS game;
+	game.ConstructConsole(320, 240, 4, 4);
+	game.Start();
 
 	return 0;
 }
